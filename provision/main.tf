b47a6460ae08c7f88cc0be6ccc997c31b68aedf7
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-06-10 00:25:46 -0400

    Adding logic for remote execution

--- provision/main.tf
+++ provision/main.tf
@@ -61,8 +61,29 @@ resource "aws_instance" "jenkins" {
   key_name        = "gocicd"  
   
   # Execute Remote commands inside the grabbed instance using "remote-exec" provisioner
-  
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y default-jre",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  } 
 
+ connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("~/Downloads/gocicd.pem")
+  }
  tags = {
     "Name"      = "Jenkins_Server"
     "Terraform" = "true"
