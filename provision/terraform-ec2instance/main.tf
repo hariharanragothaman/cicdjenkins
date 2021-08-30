087cc9fefaa9547c6a1c0fcc0e2d2ee30d9b7b4e
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-29 20:49:57 -0400

    Some minor refactoring and polishes

--- provision/terraform-ec2instance/main.tf
+++ provision/terraform-ec2instance/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = var.aws_region
 }
 
 # Configuring security groups
@@ -62,46 +62,7 @@ resource "aws_instance" "jenkins" {
   # Execute Remote commands inside the grabbed instance using "remote-exec" provisioner
   provisioner "remote-exec" {
     inline = [
-      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-      "sudo apt-get update",
-      "sudo add-apt-repository universe",
-      "sudo apt-get update",
-      "sudo apt install --assume-yes openjdk-11-jdk",
-
-      /*  Setup Docker */
-      "sudo apt-get update",
-      "sudo apt -y install docker.io",
-
-      # install remaining dependencies
-      "sudo apt -y install nginx",
-      "sudo apt -y install ufw",
-
-      /* Build Docker Image and run container in port 8080*/
-      "sudo docker build -t jenkins:jcasc jenkinsDockerSetup",
-      "sudo docker run --name jenkins --rm -p 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password jenkins:jcasc",
-
-
-      # Setup debian firewall / Network Configurations for accessing Jenkins
-      "sudo ufw status verbose",
-      "sudo ufw default deny incoming",
-      "sudo ufw default allow outgoing",
-      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
-      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
-      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
-      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
-      "sudo apt-get -y install iptables-persistent",
-      "sudo ufw allow 8080",
-      "sudo ufw allow ssh",
-      "sudo ufw allow 22",
-      "sudo ufw allow 80",
-      "sudo yes | ufw enable",
-
-      # Update Nginx Configuration
-      "sudo rm -f /etc/nginx/sites-enabled/default",
-      "sudo cp -f /tmp/jenkins-proxy /etc/nginx/sites-enabled",
-      "sudo service nginx restart"
-
+      "sudo apt update -qq",
     ]
   } 
 
