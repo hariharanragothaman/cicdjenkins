5bc4e28334dc252b9f3dad6268b40297499a2414
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-08-30 01:32:20 -0400

    Removing the default values from variables.tf

--- provision/terraform-ec2instance/variables.tf
+++ provision/terraform-ec2instance/variables.tf
@@ -5,16 +5,13 @@ variable "ingressrules" {
 
 variable "ec2_instance_type" {
   description = "AWS Instance Size"
-  default = "t2.micro"
 }
 
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default = "us-east-1"
 }
 
 variable "aws_security_group_name" {
   description = "AWS Security Group Name"
-  default = "Allow web traffic"
 }
